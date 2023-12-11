664accb1f5184bef7941dac73c5dd15c38073725
Abdul Samad <100025816+Samad88@users.noreply.github.com>
2023-12-11 11:57:50 +0530

    Update install_requirements.sh

--- package_requirement/install_requirements.sh
+++ package_requirement/install_requirements.sh
@@ -1,5 +1,22 @@
+# #!/bin/bash
+# python --version
+# pip install --upgrade azure-cli
+# pip install --upgrade azureml-sdk
+# pip install -r requirements.txt
+
 #!/bin/bash
+
 python --version
+
+pip install scikit-learn==0.24.2
+pip install joblib==1.1.0
+pip install numpy==1.21.0
+pip install pandas==1.3.0
+pip install pytest==7.0.0
+pip install pytest-cov==3.0.0
+pip install inference-schema[numpy-support]==1.3.2
+pip install lightgbm==3.2.1
+
 pip install --upgrade azure-cli
 pip install --upgrade azureml-sdk
-pip install -r requirements.txt
+# pip install -r requirements.txt
